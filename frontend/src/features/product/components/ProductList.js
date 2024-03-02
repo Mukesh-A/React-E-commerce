281ca877441a9ca50048c8497b5f3550c2738212
Mukesh-a <mukeshabhi08@gmail.com>
2024-03-02 19:23:43 +0530

    Done

--- frontend/src/features/product/components/ProductList.js
+++ frontend/src/features/product/components/ProductList.js
@@ -471,6 +471,10 @@ function ProductGrid({ products, status }) {
                 >
                   <div className="min-h-60 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-72">
                     <img
+                      loading="lazy"
+                      role="presentation"
+                      decoding="async"
+                      fetchpriority="high"
                       src={product && product?.thumbnail}
                       alt={product.title}
                       className="h-full w-full object-cover object-center lg:h-full lg:w-full"
