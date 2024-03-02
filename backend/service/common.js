4610703f44fe246d8c65d2b797f3bae0b8572cbc
Mukesh-a <mukeshabhi08@gmail.com>
2024-03-02 14:34:10 +0530

    chnage

--- backend/service/common.js
+++ backend/service/common.js
@@ -12,7 +12,7 @@ exports.cookieExtractor = function (req) {
   if (req && req.cookies) {
     token = req.cookies["jwt"];
   }
-  // token =
-  //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZTA2OGMzYjA3ZDRlZmIwZTNkZmE5YSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzA5MjA1NzAwfQ.J3tCasPscQZlBbcAAtCDP7Yp9l9Zaqrgtb2_1zuON1s";
+  token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZTFhYTFjNDdlNTE5OTQ2ODdjMDA2YyIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzA5MzY4MDU2fQ.Oyt7KkkkssDl9nGrSz-5HWwVxFXZWfV3yl0j0MNwu2k";
   return token;
 };
