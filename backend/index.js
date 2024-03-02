7670405ff6dd095efdd0dc2aa66e5f17ee0e29f5
Mukesh-a <mukeshabhi08@gmail.com>
2024-03-02 15:29:15 +0530

    test

--- backend/index.js
+++ backend/index.js
@@ -26,12 +26,8 @@ const { isAuth, sanitizerUser, cookieExtractor } = require("./service/common");
 const { Order } = require("./model/Order");
 const path = require("path");
 
-
-
 // mongoose.set("strictQuery", true);
 // Webhook
-
-// TODO: we will capture actual order after deploying out server live on public URL
 
 const endpointSecret = process.env.ENDPOINT_SECRET;
 
@@ -54,7 +50,6 @@ server.post(
     switch (event.type) {
       case "payment_intent.succeeded":
         const paymentIntentSucceeded = event.data.object;
-        console.log({ paymentIntentSucceeded });
 
         const order = await Order.findById(
           paymentIntentSucceeded.metadata.orderId
@@ -62,7 +57,6 @@ server.post(
         order.paymentStatus = "received";
         await order.save();
 
-        // Then define and call a function to handle the event payment_intent.succeeded
         break;
       // ... handle other event types
       default:
@@ -74,39 +68,33 @@ server.post(
   }
 );
 
-//jwt
-
+// JWT options
 
 const opts = {};
 opts.jwtFromRequest = cookieExtractor;
 opts.secretOrKey = process.env.JWT_SECRET_KEY;
 
+//middlewares
+
 server.use(express.static(path.resolve(__dirname, "build")));
 server.use(cookieParser());
-
 server.use(
   session({
     secret: process.env.SESSION_KEY,
-    resave: false,
-    saveUninitialized: false,
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
   })
 );
 server.use(passport.authenticate("session"));
-// we are using exposedHeaders because in frontend we have used X-Total-Count from the header request to count number of items from the request . so when we are doing that in backend we have to use exposedHeaders to expose the X-Total-Count so properly the pagination will be displayed
-
 server.use(
   cors({
     exposedHeaders: ["X-Total-Count"],
   })
 );
-// server.use(express.raw({ type: "application/json" }));
-server.use(express.json());
-
-//middleware
-
-//passport
+server.use(express.json()); // to parse req.body
 
 server.use("/products", isAuth(), productRouters.router);
+// we can also use JWT token for client-only auth
 server.use("/categories", isAuth(), categoriesRouters.router);
 server.use("/brands", isAuth(), brandRouters.router);
 server.use("/users", isAuth(), usersRouters.router);
@@ -114,15 +102,12 @@ server.use("/auth", authRouters.router);
 server.use("/cart", isAuth(), cartRouters.router);
 server.use("/orders", isAuth(), orderRouters.router);
 
+// this line we add to make react router work in case of other routes doesnt match
 server.get("*", (req, res) =>
   res.sendFile(path.resolve("build", "index.html"))
 );
 
-// server.get("/", (req, res) => {
-//   res.send({ status: "success" });
-// });
-
-// passport strategies
+// Passport Strategies
 passport.use(
   "local",
   new LocalStrategy({ usernameField: "email" }, async function (
@@ -131,12 +116,13 @@ passport.use(
     done
   ) {
     // by default passport uses username
+    console.log({ email, password });
     try {
-      const user = await User.findOne({ email: email }).exec();
+      const user = await User.findOne({ email: email });
+      console.log(email, password, user);
       if (!user) {
-        done(null, false, { message: "invalid credentials" });
+        return done(null, false, { message: "invalid credentials" }); // for safety
       }
-
       crypto.pbkdf2(
         password,
         user.salt,
@@ -151,8 +137,7 @@ passport.use(
             sanitizerUser(user),
             process.env.JWT_SECRET_KEY
           );
-
-          done(null, { id: user.id, role: user.role, token }); // this is send to serializer
+          done(null, { id: user.id, role: user.role, token }); // this lines sends to serializer
         }
       );
     } catch (err) {
@@ -161,14 +146,13 @@ passport.use(
   })
 );
 
-// jwt strategies
 passport.use(
   "jwt",
   new JwtStrategy(opts, async function (jwt_payload, done) {
     try {
       const user = await User.findById(jwt_payload.id);
       if (user) {
-        return done(null, sanitizerUser(user)); //this calls serializer
+        return done(null, sanitizerUser(user)); // this calls serializer
       } else {
         return done(null, false);
       }
@@ -178,24 +162,24 @@ passport.use(
   })
 );
 
-// this create session variable req.user on being called from callback
+// this creates session variable req.user on being called from callbacks
 passport.serializeUser(function (user, cb) {
-  console.log("serializeUser", user);
   process.nextTick(function () {
     return cb(null, { id: user.id, role: user.role });
   });
 });
 
-// this create session variable req.user on being called from authorized request
-passport.deserializeUser(function (user, cb) {
-  console.log("de-serializeUser", user);
+// this changes session variable req.user when called from authorized request
 
+passport.deserializeUser(function (user, cb) {
   process.nextTick(function () {
     return cb(null, user);
   });
 });
 
-//payment
+// Payments
+
+// This is your test secret API key.
 const stripe = require("stripe")(process.env.STRIPE_SERVER_KEY);
 
 server.post("/create-payment-intent", async (req, res) => {
@@ -205,17 +189,6 @@ server.post("/create-payment-intent", async (req, res) => {
   const paymentIntent = await stripe.paymentIntents.create({
     amount: totalAmount * 100, // for decimal compensation
     currency: "inr",
-    description: "Software development services",
-    shipping: {
-      name: "Jenny Rosen",
-      address: {
-        line1: "510 Townsend St",
-        postal_code: "98140",
-        city: "San Francisco",
-        state: "CA",
-        country: "US",
-      },
-    },
     automatic_payment_methods: {
       enabled: true,
     },
@@ -229,17 +202,13 @@ server.post("/create-payment-intent", async (req, res) => {
   });
 });
 
-// Database connection
-const MONGODB_URL = process.env.MONGO_URL;
-const port = process.env.PORT;
-mongoose
-  .connect(MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    server.listen(port, () => {
-      console.log(`Server Running in port ${port}`);
-    });
-  })
-  .catch((err) => console.log(err));
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URL);
+  console.log("database connected");
+}
+
+server.listen(process.env.PORT, () => {
+  console.log("server started");
+});
